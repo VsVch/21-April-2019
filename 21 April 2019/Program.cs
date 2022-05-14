eeabdc34d0f578a40ca3ab28a7453dcb16b1c94b
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-05-14 09:28:44 +0300

    Add project files.

---
+++ 21 April 2019/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _21_April_2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
